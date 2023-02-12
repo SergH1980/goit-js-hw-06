04459e4dee344817225c72f3310d3311f78ace46
SergH1980 <serg.galaka@gmail.com>
2023-02-12 18:31:48 +0200

    assignment 3

--- js/task-02.js
+++ js/task-02.js
@@ -7,22 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 
-const liEl = document.createElement(`li`);
 const ingredientsList = document.querySelector(`#ingredients`);
-const Ingredient = ingredients.map((element) => {
-  liEl.textContent = element;
-  liEl.classList.add(`item`);
-  console.log(liEl);
+
+const ingredientEl = ingredients.map((item) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = item;
+  liEl.classList.add("item");
   return liEl;
 });
 
-ingredientsList.appendChild(liEl);
-
-// Напиши скрипт, який для кожного елемента масиву ingredients:
-
-// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement(). +
-// Додасть назву інгредієнта як його текстовий вміст.
-// Додасть елементу клас item.
-// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
-
-//  `<li>${element}</li>`).join(``)
+ingredientsList.append(...ingredientEl);

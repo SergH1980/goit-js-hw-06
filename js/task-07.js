facd43be6c8e090c4b1db32888a35d06464993c2
SergH1980 <serg.galaka@gmail.com>
2023-02-13 23:16:53 +0200

    removed console.log task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,5 @@
 const sizeControl = document.querySelector(`#font-size-control`);
 const textSpan = document.querySelector(`#text`);
-console.log(sizeControl.value);
 textSpan.style.fontSize = `${sizeControl.value}px`;
 
 sizeControl.addEventListener(`input`, toChangeFontSize);
